d5a369ca632ae2cd988a6e28d13d1e9bfecc6e41
NagosC <121381818+NagosC@users.noreply.github.com>
2023-03-21 09:32:45 -0300

    adicionando cors

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const port = 3334;
-
-app.listen(port, () => {
-  console.log(`escutando na porta ${port}`);
-  console.log(`http//localhost:${port}`);
-});
--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import { resolve } from 'path';
-import homeRoutes from './src/routes/homeRoutes';
-import userRoutes from './src/routes/userRoutes';
-import tokenRoutes from './src/routes/tokenRoutes';
-import alunoRoutes from './src/routes/alunoRoutes';
-import photoRoutes from './src/routes/photoRoutes';
-import './src/database';
+import cors from 'cors';
+import helmet from 'helmet';
+import homeRoutes from './routes/homeRoutes';
+import userRoutes from './routes/userRoutes';
+import tokenRoutes from './routes/tokenRoutes';
+import alunoRoutes from './routes/alunoRoutes';
+import photoRoutes from './routes/photoRoutes';
+import './database';
+
+const whiteList = [
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 dotenv.config();
 
@@ -18,6 +34,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
